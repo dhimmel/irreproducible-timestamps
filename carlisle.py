e7ee59f319737953d96a76be6d7cbbd3635d9c51
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-03-07 13:15:41 -0500

    Create function for performing Carlisle method

---
+++ carlisle.py
@@ -0,0 +1,29 @@
+import hashlib
+
+import bitcoin
+
+
+def carlisle_method(data, compress=False):
+    """
+    Use the Carlisle method to generate a bitcoin address the hash of data.
+    Returns a dictionary with address and generation information.
+    """
+    # Compute the sha256 hash of the data
+    sha256_hash = hashlib.sha256(data)
+    private_key = sha256_hash.hexdigest()
+
+    # Generate public key
+    public_key = bitcoin.privkey_to_pubkey(private_key)
+    if compress:
+        public_key = bitcoin.compress(public_key)
+
+    # Generate bitcoin address
+    address = bitcoin.pubkey_to_address(public_key)
+
+    return {
+        'private_key': private_key,
+        'public_key': public_key,
+        'compressed': compress,
+        'address': address,
+        'url ': 'https://blockchain.info/address/{}'.format(address),
+    }
